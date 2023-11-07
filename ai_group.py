502d785b77c7c08aab001ec9d4ac237196de3220
Himanshu Sharma <himanshusharma96@gmail.com>
2023-11-07 00:51:01 -0500

    Mostly working. Need to iron out issues

--- ai_group.py
+++ ai_group.py
@@ -1,7 +1,12 @@
+QUERY_LOG_FILE = 'query_logs.log'
+
 import discord
 from discord import app_commands
 from discord.ext import commands
 from utils.utils import createUrl
+
+import logging
+from logging.handlers import RotatingFileHandler
 
 import aiohttp
 
@@ -9,21 +14,39 @@ class AIgroup(app_commands.Group):
     """Manage general commands"""
     def __init__(self, bot: commands.Bot, config: dict[str, any]):
         super().__init__()
+
+        # separate logger for debugging queries.
+        my_handler = RotatingFileHandler(QUERY_LOG_FILE,
+            mode = 'a',
+            maxBytes = 5 * 1024 * 1024,
+            backupCount = 2,
+            encoding = None,
+            delay = 0)
+        my_handler.setLevel(logging.INFO)
+
+        q_log = logging.getLogger('query')
+        q_log.setLevel(logging.INFO)
+        q_log.addHandler(my_handler)
+
+        self.query_logger = q_log
+
         self.name = "ai"
         self.bot = bot
         self.config = config
 
     @app_commands.command(name = 'chat')
-    async def hello(self, interaction: discord.Interaction):
+    async def hello(self, interaction: discord.Interaction, query: str):
         # await interaction.response.send_message('Hello')
 
         async with aiohttp.ClientSession() as session:
+            request_data = {"query": query}
             service_url = createUrl(self.config['model_server']['chat_server'], self.config['model_server']['chat_port'], self.config['model_server']['chat_uri'])
-            async with session.get(service_url) as r:
+            async with session.post(service_url, data = request_data) as r:
                 if r.status == 200:
                     js = await r.json()
-                    # TODO: log the packet here
-                    return await interaction.response.send_message(js['message'])
+                    chat_msg = js.get('message', 'Looks like the server sent something weird. Gotta protect you from it.')
+                    self.query_logger.info(f'Query: {query}, response: {js}')
+                    return await interaction.response.send_message(chat_msg)
                     # await channel.send(js['file'])
                 else:
                     return await interaction.response.send_message('Uff kuch to galti hui...')
--- ai_server/ai_app.py
+++ ai_server/ai_app.py
@@ -1,7 +1,33 @@
+import os
 from flask import Flask, render_template, request
+from ctransformers import AutoModelForCausalLM
+
+llm_obj = None
+
+def start_model():
+	# Set gpu_layers to the number of layers to offload to GPU. Set to 0 if no GPU acceleration is available on your system.
+	print(f'load once')
+	global llm_obj
+	llm_obj = AutoModelForCausalLM.from_pretrained("TheBloke/Mistral-7B-Instruct-v0.1-GGUF",
+		model_file="mistral-7b-instruct-v0.1.Q3_K_L.gguf",
+		model_type="mistral",
+		gpu_layers=50)
 
 
-app = Flask(__name__)
+# run after flask startup but before first request.
+class ModelRunnerFlask(Flask):
+	def run(self, host=None, port=None, debug=None, load_dotenv=True, **options):
+		if not (app.debug or os.environ.get("FLASK_ENV") == "development") or os.environ.get("WERKZEUG_RUN_MAIN") == "true":
+		# if os.getenv('WERKZEUG_RUN_MAIN') == 'true':
+		# the app could reload, but this won't be executed again
+			with self.app_context():
+				# TODO: make this multi threaded?
+				self.logger.info("Loading the model")
+				start_model()
+
+		super(ModelRunnerFlask, self).run(host=host, port=port, debug=debug, load_dotenv=load_dotenv, **options)
+
+app = ModelRunnerFlask(__name__)
 
 ENV = 'dev'
 
@@ -21,10 +47,22 @@ def index():
 def text_chat():
 	# TODO: Auth
 	if request.method == 'POST' or True:
-		message = "Yo boi, how you doing?"
+		print(f'values: {request.values}, args:{request.args}')
+		content = request.values
+
+		query = content.get('query', default = '')
+		# TODO: handle blank
+		# "List the top 20 most famous presidents of the United states."
+		print(f'query: {query}')
+		message = "Yo boi, how you doing? Looks like my synthetic brain is not active yet."
+		bot_name = "None"
+		if llm_obj:
+			bot_name = llm_obj.model_type
+			message = llm_obj(query)
+			print(f'message: {message}')
 		# return render_template('insert_success.html', message='Application Rejected!')
 		return {
-			"bot_name": "none",
+			"bot_name": bot_name,
 			"message": message
 		}
 
@@ -33,11 +71,14 @@ def text_chat():
 def image_chat():
 	# TODO: Auth
 	if request.method == 'POST':
-		# return render_template('insert_success.html', message='Application Rejected!')
+		content = request.values
+
+		query = content.get('query', default = '')
+		# TODO: Handle blank
 		return {
 			"image": "asdf"
 		}
 
-
 if __name__ == '__main__':
+	print(f'call me once')
 	app.run()
