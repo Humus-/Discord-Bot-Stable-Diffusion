d4b87deb0d6c2bb99132e52cf9c083b3705fbc99
Himanshu Sharma <himanshusharma96@gmail.com>
2023-11-19 23:17:02 -0500

    Chat flag. Not tested

--- ai_server/ai_app.py
+++ ai_server/ai_app.py
@@ -3,6 +3,13 @@ from flask import Flask, render_template, request
 from ctransformers import AutoModelForCausalLM
 
 llm_obj = None
+# gonna make the flags as a config parameter later. Maybe even a pub sub thing for the server
+FLAG_CHAT = 'chat'
+FLAG_IMAGE = 'image'
+MODEL_FALGS = {
+	FLAG_CHAT: True
+	FLAG_IMAGE: False
+}
 
 def start_model():
 	# Set gpu_layers to the number of layers to offload to GPU. Set to 0 if no GPU acceleration is available on your system.
@@ -45,25 +52,34 @@ def index():
 @app.route('/text_chat', methods=['POST', 'GET'])
 def text_chat():
 	# TODO: Auth
-	if request.method == 'POST' or True:
-		print(f'values: {request.values}, args:{request.args}')
-		content = request.values
+	if request.method == 'POST':
+		pass
 
-		query = content.get('query', default = '')
-		# TODO: handle blank
-		# "List the top 20 most famous presidents of the United states."
-		print(f'query: {query}')
-		message = "Yo boi, how you doing? Looks like my synthetic brain is not active yet."
-		bot_name = "None"
-		if llm_obj:
-			bot_name = llm_obj.model_type
-			message = llm_obj(query)
-			print(f'message: {message}')
-		# return render_template('insert_success.html', message='Application Rejected!')
+	if not MODEL_FALGS[FLAG_CHAT]:
+		message = "This service is not loaded on the current server."
 		return {
-			"bot_name": bot_name,
-			"message": message
-		}
+		"bot_name": 'Admin',
+		"message": message
+	}
+
+	print(f'values: {request.values}, args:{request.args}')
+	content = request.values
+
+	query = content.get('query', default = '')
+	# TODO: handle blank
+	# "List the top 20 most famous presidents of the United states."
+	print(f'query: {query}')
+	message = "Yo boi, how you doing? Looks like my synthetic brain is not active yet."
+	bot_name = "None"
+	if llm_obj:
+		bot_name = llm_obj.model_type
+		message = llm_obj(query)
+		print(f'message: {message}')
+	# return render_template('insert_success.html', message='Application Rejected!')
+	return {
+		"bot_name": bot_name,
+		"message": message
+	}
 
 
 @app.route('/img_chat', methods=['POST'])
